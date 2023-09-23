ded885598f28cfedd6e6ff365844896002bad698
Galina684 <galina684@gmail.com>
2023-09-23 17:10:07 +0300

    Домашка Коллекции и сеты upd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.*;
+
 public class Main {
+    static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    static List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        oddNumbers(nums);
+        evenNumbers(nums);
+        uniqueWords(strings);
+        duplicateWords(strings);
+
+    }
+
+    private static void oddNumbers(List<Integer> nums) {
+        System.out.println("Задача 1. Нечетные числа");
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    private static void evenNumbers(List<Integer> nums) {
+        System.out.println("Задача 2. Четные числа");
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    private static void uniqueWords(List<String> strings) {
+        System.out.println("Задача 3. Уникальные слова");
+        Set<String> uniqueWords = new HashSet<>(strings);
+        System.out.println(uniqueWords);
+    }
+
+    private static void duplicateWords(List<String> strings) {
+        System.out.println("Задача 4. Дубли слов");
+        Map<String, Integer> wordsMap = new HashMap<>();
+        for (String word : strings) {
+            if (wordsMap.containsKey(word)) {
+                wordsMap.put(word, wordsMap.get(word) + 1);
+            } else {
+                wordsMap.put(word, 1);
+            }
+        }
+        System.out.println(wordsMap);
     }
 }
